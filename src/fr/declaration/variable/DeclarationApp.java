611e0e5840a008d022c2e3b2f39f8d683a2d990f
Joachim <joachimsall8@gmail.com>
2021-04-22 14:19:20 +0200

    initialisation

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 12;
+		short b = 14;
+		int c = 15;
+		long d = 153l;
+		float e = 10f;
+		double f = 123;
+		char g = 'g';
+		boolean h = true;
+		String i = "bonjour";
+		String randomString = "VOici le résultat d'un calcul :\n 2 + 2 = 4";
+		
+		System.out.println(randomString);
+
+	}
+
+
+}
